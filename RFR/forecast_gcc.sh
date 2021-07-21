4def330e7c5f5eedcef144e44e4be90dd0f50674
Jessica Guo <jessicaguo@email.arizona.edu>
2021-07-21 00:13:35 +0000

    Edited and tested scripts for fusion models for rcc and gcc

--- RFR/forecast_gcc.sh
+++ RFR/forecast_gcc.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 Rscript 01_pull_gcc_weather.R
-python3 02_PEG_RFR0_gcc_predictions.py
-python3 03_PEG_RFR_gcc_predictions_with_interpolation.py
-python3 04_PEG_RFR2_gcc_predictions_with_weather_variables.py
-Rscript 05_submit_rfr.R
+# python3 02_PEG_RFR0_gcc_predictions.py
+# python3 03_PEG_RFR_gcc_predictions_with_interpolation.py
+# python3 04_PEG_RFR2_gcc_predictions_with_weather_variables.py
+python3 05_PEG_FUSION_0_future_predictions.py
+Rscript 06_submit_fusion.R
