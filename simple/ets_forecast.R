ace756609bcdc72924c889e5ce2e06692eb6f8f6
Jessica Guo <jessicaguo@email.arizona.edu>
2021-09-20 19:42:29 +0000

    Comment out EML-related portions

--- simple/ets_forecast.R
+++ simple/ets_forecast.R
@@ -80,135 +80,131 @@ readr::write_csv(preds, file = pred_filename)
 ## metadata
 ## from https://github.com/eco4cast/EFIstandards/blob/master/vignettes/logistic-metadata-example.Rmd
 ## TODO: Replace w/ helper functions (when available) https://github.com/eco4cast/neon4cast#generate-forecast-metadata-in-eml
-library(EML)
-library(emld)
-emld::eml_version("eml-2.2.0")
-
-
-template_eml <- EML::read_eml('https://raw.githubusercontent.com/eco4cast/neon4cast-beetles/c7e76a2585c55f6141e4b51a081edee65b8ea82a/meta/eml.xml')
-
-attributes <- tibble::tribble(
-  ~attributeName,     ~attributeDefinition,                          ~unit,                  ~formatString, ~numberType, ~definition,
-  "time",              "[dimension]{time}",                          "year",                 "YYYY-MM-DD",  "numberType", NA,
-  "siteID",            "[dimension]{depth in reservior}",            "dimensionless",         NA,           "character",  NA,
-  "gcc_90",            "[observation]{Predicted GCC}",               "proportion",            NA,           "integer",    NA,
-  "gcc_sd",            "[process_error]{observation error}",         "proportion",            NA,           "integer",    NA
-) 
-
-attributes
-attrList <- set_attributes(attributes, 
-                           col_classes = c("Date", "character", "numeric","numeric"))
-
-physical <- set_physical(pred_filename,
-                         recordDelimiter='\n')
-
-dataTable <- eml$dataTable(
-  entityName = "forecast",  
-  entityDescription = "Forecast of GCC",
-  physical = physical,
-  attributeList = attrList)
-
-me <- list(
-  individualName = list(givenName = "David", 
-                        surName = "LeBauer"),
-  electronicMailAddress = "dlebauer@arizona.edu",
-  id = "https://orcid.org/0000-0001-7228-053X")
-
-
-keywordSet <- list(
-  list(
-    keywordThesaurus = "EFI controlled vocabulary",
-    keyword = list("forecast",
-                   "phenology",
-                   "timeseries")
-  ))
-
-abstract_text <- 'a secret way of predicting gcc'
-
-plant_cover <- readr::read_csv('plant_cover/plant_cover.csv')
-
-neon_sites <- c("HARV", "BART", "SCBI", "STEI", "UKFS", "GRSM", "DELA", "CLBJ")
-coverage <- 
-  set_coverage(begin = now, 
-               end = end,
-               geographicDescription = paste(c("NEON Sites:", neon_sites), sep = ' ', collapse = ' '),
-               west = min(plant_cover$lon), east = max(plant_cover$lon), 
-               north = max(plant_cover$lat), south = min(plant_cover$lat)
-  )
-
-dataset = eml$dataset(
-  title = "PEG phenology prediction",
-  creator = me,
-  contact = list(references="https://orcid.org/0000-0001-7228-053X"),
-  pubDate = Sys.Date(),
-  intellectualRights = "http://www.lternet.edu/data/netpolicy.html.",
-  abstract =  "A Secret Sauce Prediction",
-  dataTable = dataTable,
-  keywordSet = keywordSet,
-  coverage = coverage
-)
-
-forecast_iteration_id = as.numeric(now)
-additionalMetadata <- eml$additionalMetadata(
-  metadata = list(
-    forecast = list(
-      ## Basic elements
-      timestep = "1 day", ## should be udunits parsable; already in coverage -> temporalCoverage?
-      forecast_horizon = "35 days",
-      forecast_issue_time = now,
-      forecast_iteration_id = forecast_iteration_id,
-      forecast_project_id = "PEG",
-      metadata_standard_version = "0.3",
-      model_description = list(
-        forecast_model_id = 'Secret Sauce',
-        name = "mean of prev. obs on date + inflated error",
-        type = "process-based",
-        repository = "https://github.com/genophenoenvo/EFI-phenology"
-      ),
-      ## MODEL STRUCTURE & UNCERTAINTY CLASSES
-      initial_conditions = list(
-        # Possible values: absent, present, data_driven, propagates, assimilates
-        status = "data_driven",
-        # Number of parameters / dimensionality
-        complexity = 1  ## [species 1, species 2] per depth
-      ),
-      drivers = list(
-        status = "data_driven"
-      ),
-      parameters = list(
-        status = "present",
-        complexity = 1  ## [r, K, alpha] x 2 spp
-      ),
-      random_effects = list(
-        status = "absent"
-      ),
-      process_error = list(
-        status = "absent"
-      ),
-      obs_error = list(
-        status = "absent"
-      )
-    ) # forecast
-  ) # metadata
-) # eml$additionalMetadata
-
-my_eml <- eml$eml(dataset = dataset,
-                  additionalMetadata = additionalMetadata,
-                  packageId = forecast_iteration_id , 
-                  system = "datetime"  ## system used to generate packageId
-)
-
-eml_validate(my_eml)
-meta_data_filename <- gsub(pattern = 'csv', replacement = 'xml', pred_filename)
-write_eml(my_eml, meta_data_filename)
+# library(EML)
+# library(emld)
+# emld::eml_version("eml-2.2.0")
+# 
+# 
+# template_eml <- EML::read_eml('https://raw.githubusercontent.com/eco4cast/neon4cast-beetles/c7e76a2585c55f6141e4b51a081edee65b8ea82a/meta/eml.xml')
+# 
+# attributes <- tibble::tribble(
+#   ~attributeName,     ~attributeDefinition,                          ~unit,                  ~formatString, ~numberType, ~definition,
+#   "time",              "[dimension]{time}",                          "year",                 "YYYY-MM-DD",  "numberType", NA,
+#   "siteID",            "[dimension]{depth in reservior}",            "dimensionless",         NA,           "character",  NA,
+#   "gcc_90",            "[observation]{Predicted GCC}",               "proportion",            NA,           "integer",    NA,
+#   "gcc_sd",            "[process_error]{observation error}",         "proportion",            NA,           "integer",    NA
+# ) 
+# 
+# attributes
+# attrList <- set_attributes(attributes, 
+#                            col_classes = c("Date", "character", "numeric","numeric"))
+# 
+# physical <- set_physical(pred_filename,
+#                          recordDelimiter='\n')
+# 
+# dataTable <- eml$dataTable(
+#   entityName = "forecast",  
+#   entityDescription = "Forecast of GCC",
+#   physical = physical,
+#   attributeList = attrList)
+# 
+# me <- list(
+#   individualName = list(givenName = "David", 
+#                         surName = "LeBauer"),
+#   electronicMailAddress = "dlebauer@arizona.edu",
+#   id = "https://orcid.org/0000-0001-7228-053X")
+# 
+# 
+# keywordSet <- list(
+#   list(
+#     keywordThesaurus = "EFI controlled vocabulary",
+#     keyword = list("forecast",
+#                    "phenology",
+#                    "timeseries")
+#   ))
+# 
+# abstract_text <- 'a secret way of predicting gcc'
+# 
+# plant_cover <- readr::read_csv('plant_cover/plant_cover.csv')
+# 
+# neon_sites <- c("HARV", "BART", "SCBI", "STEI", "UKFS", "GRSM", "DELA", "CLBJ")
+# coverage <- 
+#   set_coverage(begin = now, 
+#                end = end,
+#                geographicDescription = paste(c("NEON Sites:", neon_sites), sep = ' ', collapse = ' '),
+#                west = min(plant_cover$lon), east = max(plant_cover$lon), 
+#                north = max(plant_cover$lat), south = min(plant_cover$lat)
+#   )
+# 
+# dataset = eml$dataset(
+#   title = "PEG phenology prediction",
+#   creator = me,
+#   contact = list(references="https://orcid.org/0000-0001-7228-053X"),
+#   pubDate = Sys.Date(),
+#   intellectualRights = "http://www.lternet.edu/data/netpolicy.html.",
+#   abstract =  "A Secret Sauce Prediction",
+#   dataTable = dataTable,
+#   keywordSet = keywordSet,
+#   coverage = coverage
+# )
+# 
+# forecast_iteration_id = as.numeric(now)
+# additionalMetadata <- eml$additionalMetadata(
+#   metadata = list(
+#     forecast = list(
+#       ## Basic elements
+#       timestep = "1 day", ## should be udunits parsable; already in coverage -> temporalCoverage?
+#       forecast_horizon = "35 days",
+#       forecast_issue_time = now,
+#       forecast_iteration_id = forecast_iteration_id,
+#       forecast_project_id = "PEG",
+#       metadata_standard_version = "0.3",
+#       model_description = list(
+#         forecast_model_id = 'Secret Sauce',
+#         name = "mean of prev. obs on date + inflated error",
+#         type = "process-based",
+#         repository = "https://github.com/genophenoenvo/EFI-phenology"
+#       ),
+#       ## MODEL STRUCTURE & UNCERTAINTY CLASSES
+#       initial_conditions = list(
+#         # Possible values: absent, present, data_driven, propagates, assimilates
+#         status = "data_driven",
+#         # Number of parameters / dimensionality
+#         complexity = 1  ## [species 1, species 2] per depth
+#       ),
+#       drivers = list(
+#         status = "data_driven"
+#       ),
+#       parameters = list(
+#         status = "present",
+#         complexity = 1  ## [r, K, alpha] x 2 spp
+#       ),
+#       random_effects = list(
+#         status = "absent"
+#       ),
+#       process_error = list(
+#         status = "absent"
+#       ),
+#       obs_error = list(
+#         status = "absent"
+#       )
+#     ) # forecast
+#   ) # metadata
+# ) # eml$additionalMetadata
+# 
+# my_eml <- eml$eml(dataset = dataset,
+#                   additionalMetadata = additionalMetadata,
+#                   packageId = forecast_iteration_id , 
+#                   system = "datetime"  ## system used to generate packageId
+# )
+# 
+# eml_validate(my_eml)
+# meta_data_filename <- gsub(pattern = 'csv', replacement = 'xml', pred_filename)
+# write_eml(my_eml, meta_data_filename)
 
 ## TODO: replace with neon4cast::submit(pred_filename, metadata = meta_data_filename) 
 ## https://github.com/eco4cast/neon4cast#submit-a-forecast
 aws.s3::put_object(pred_filename, 
-                   bucket = "submissions", 
-                   region="data", 
-                   base_url = "ecoforecast.org")
-aws.s3::put_object(meta_data_filename, 
                    bucket = "submissions", 
                    region="data", 
                    base_url = "ecoforecast.org")
